32d19dae3aebbc0071a9eb7e369a5beaf2c31c28
John Kozak <jk@thameslighter.net>
2022-09-23 09:40:06 +0100

    init

---
+++ shell.nix
@@ -0,0 +1,11 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "malaya-plugin-xmpp--build-env";
+  buildInputs = [ systemd nodejs-18_x jq ];
+  shellHook = ''
+export PATH=${builtins.getEnv "PWD"}/node_modules/.bin:$PATH
+#export HISTFILE=${builtins.getEnv "PWD"}/.bash_history
+#history -c
+#history -r $HISTFILE
+'';
+}
